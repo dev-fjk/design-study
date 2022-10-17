400bc948563e8c3c0f293ea49e3a448adaff6de1
dev-fjk <fjk.develop@gmail.com>
2022-10-17 22:15:56 +0900

    add : hero

---
+++ src/seito/assets/js/app.js
@@ -0,0 +1,5 @@
+// https://app.netlify.com/teams/dev-fjk/overview
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/js/particles.json', function () {
+    console.log('callback - particles.js config loaded');
+});
